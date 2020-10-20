fda8ab4b7a7f5d860915dd7256331b7759dc89f1
MikuroXina <ryosukadnak@gmail.com>
2020-10-20 20:54:58 +0900

    :sparkles: Add editor view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
+import MonacoEditor from "@monaco-editor/react";
 
-const Index: NextPage = () => <h1>Nyansphere</h1>;
+const Index: NextPage = () => (
+  <>
+    <h1>Nyansphere</h1>
+    <main>
+      <MonacoEditor
+        language="cpp"
+        value={[
+          "#include <iostream>",
+          "",
+          "int main() {",
+          '  std::cout << "Hello, World!\\n";',
+          "}",
+        ].join("\n")}
+      />
+    </main>
+    <style jsx>{`
+      h1 {
+        position: fixed:
+        z-index: 5;
+      }
+      main {
+        height: 95vh;
+      }
+    `}</style>
+  </>
+);
 
 export default Index;
-
