c187db2f25fda9169370c9e34145eba50d074d7e
MikuroXina <ryosukadnak@gmail.com>
2020-10-21 21:43:11 +0900

    :pencil: Make index wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,19 @@
 import type { NextPage } from "next";
-import { CodeBody } from "../components/code-body";
-import { JudgeStatus } from "../components/judge-status";
+import { InternalLink } from "../components/internal-link";
 import { Template } from "../components/template";
 
-const exampleText = (
-  <>
-    <h1>Hello, World!</h1>
-    動作確認をしましょう。
-    <h2>入力</h2>
-    なし
-    <h2>出力</h2>
-    <code>Hello, World!</code> と改行を合わせて出力する.
-  </>
-);
-
-const exampleCode = [
-  "#include <iostream>",
-  "",
-  "int main() {",
-  '  std::cout << "Hello, World!\\n";',
-  "}",
-].join("\n");
-
 const Index: NextPage = () => (
-  <Template headerItem={<JudgeStatus state="no-submit" />}>
-    <CodeBody text={exampleText} defaultCode={exampleCode} />
+  <Template
+    headerItems={[
+      <InternalLink href="/subjects" key="subjects">
+        Subjects
+      </InternalLink>,
+      <InternalLink href="/submits" key="submits">
+        Submits
+      </InternalLink>,
+    ]}
+  >
+    Work in Progress
   </Template>
 );
 
