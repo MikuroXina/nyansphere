8b8292e741c448708f9587b2d9586f3b7206b774
MikuroXina <ryosukadnak@gmail.com>
2020-10-24 17:08:49 +0900

    :pencil2: Fix message and refactor

--- src/components/judge-status.tsx
+++ src/components/judge-status.tsx
@@ -2,17 +2,15 @@ import type { FC } from "react";
 import { JudgeState } from "../lib/judge-state";
 
 const messages: Readonly<Record<JudgeState, string>> = {
-  "no-submit": "",
+  "no-submit": "未提出",
   judging: "判定中",
   wrong: "不正解",
   accepted: "正解",
 } as const;
 
 export const JudgeStatus: FC<{ state: JudgeState }> = ({ state }) => (
-  <>
-    <span className="status" data-state={state}>
-      {messages[state]}
-    </span>
+  <span className="status" data-state={state}>
+    {`提出状態: ${messages[state]}`}
     <style jsx>{`
       .status[data-state="judging"] {
         color: orange;
@@ -24,5 +22,5 @@ export const JudgeStatus: FC<{ state: JudgeState }> = ({ state }) => (
         color: green;
       }
     `}</style>
-  </>
+  </span>
 );
