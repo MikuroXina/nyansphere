2b5b0896c36b07b94df079eb56277637e1c44326
MikuroXina <ryosukadnak@gmail.com>
2020-10-24 18:06:31 +0900

    :fire: Make unoptional

--- src/lib/subject.ts
+++ src/lib/subject.ts
@@ -2,9 +2,9 @@ export interface Subject {
   id: string;
   title: string;
   desc: string;
-  defaultCode?: string;
-  inputDesc?: string;
-  outputDesc?: string;
+  defaultCode: string;
+  inputDesc: string;
+  outputDesc: string;
   constraintDesc?: string;
 }
 
