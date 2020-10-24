651f5e0489bce3b903063c25e71ebbe7ec8343b4
MikuroXina <ryosukadnak@gmail.com>
2020-10-24 17:07:47 +0900

    :recycle: Integrate JudgeStatus

--- src/components/submit-list.tsx
+++ src/components/submit-list.tsx
@@ -1,34 +1,8 @@
 import type { FC } from "react";
 import { InternalLink } from "./internal-link";
-import type { JudgeState } from "../lib/judge-state";
+import { JudgeStatus } from "./judge-status";
 import { List } from "./list";
 import type { PartialSubmit } from "../lib/submit";
-
-const JudgeText: FC<{ state: JudgeState }> = ({ state }) => (
-  <>
-    <span data-state={state}>
-      {
-        {
-          "no-submit": "未提出",
-          judging: "採点中",
-          wrong: "不正解",
-          accepted: "正解",
-        }[state]
-      }
-    </span>
-    <style jsx>{`
-      span[data-state="judging"] {
-        color: orange;
-      }
-      span[data-state="wrong"] {
-        color: red;
-      }
-      span[data-state="accepted"] {
-        color: green;
-      }
-    `}</style>
-  </>
-);
 
 const SubmitColumn: FC<{ datum: PartialSubmit }> = ({
   datum: { title, date, judge },
@@ -36,7 +10,7 @@ const SubmitColumn: FC<{ datum: PartialSubmit }> = ({
   <>
     <span>{title}</span>
     <span>{date}</span>
-    <JudgeText state={judge} />
+    <JudgeStatus state={judge} />
     <style jsx>{`
       span {
         margin-right: 1rem;
